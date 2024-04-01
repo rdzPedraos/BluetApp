a10b69c40721a461d3b542a0fcedec69e6b5d3d9
rdzPedraos <rdzpedraos@gmail.com>
2024-04-01 03:11:22 -0500

    Add Joystick component

---
+++ src/components/Joystick.jsx
@@ -0,0 +1,54 @@
+import React, { useMemo } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { useJoystick } from '@/hooks';
+
+export default function Joystick({
+    radius = 100,
+    color = '#000',
+    addDebug = false,
+}) {
+    const { position, debug, render } = useJoystick(radius, addDebug);
+    const styles = useMemo(() => generateStyles(radius, color), [radius, color]);
+
+    const movementStyles = useMemo(() => {
+        return { transform: [{ translateX: position.x }, { translateY: position.y }] };
+    }, [position]);
+
+    return (
+        <View style={styles.base} {...render}>
+            <View style={[styles.joystick, movementStyles]} />
+
+            {debug && (
+                <View style={{ position: 'absolute', backgroundColor: '#f0f0f055' }}>
+                    <Text>{JSON.stringify(debug, null, 2)}</Text>
+                </View>
+            )}
+        </View>
+    );
+};
+
+const generateStyles = (radius, color) => {
+    const baseSize = radius * 2;
+    const joystickSize = radius;
+
+    return StyleSheet.create({
+        base: {
+            position: 'relative',
+            width: baseSize,
+            height: baseSize,
+            borderRadius: baseSize,
+            backgroundColor: color,
+            opacity: 0.5,
+        },
+        joystick: {
+            width: joystickSize,
+            height: joystickSize,
+            borderRadius: joystickSize,
+            position: 'absolute',
+            top: joystickSize / 2,
+            left: joystickSize / 2,
+            backgroundColor: color,
+        },
+    });
+}
+
