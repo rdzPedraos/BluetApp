7bd6b6bbeb446c501e8d1677a3b7e03e2da00a92
rdzPedraos <rdzpedraos@gmail.com>
2024-06-03 16:54:44 -0500

    refactor: structure images

---
+++ src/constants/images.js
@@ -0,0 +1,2 @@
+export const BRAND_LOGO = require('@/assets/brand.png');
+export const LOGO = require('@/assets/logo.png');
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,4 @@
 export { COLORS } from './colors';
 export { FONT_SIZES } from './sizes';
 export { Ionicons as ICONS } from '@expo/vector-icons';
-export const BRAND_LOGO = require('@/assets/brand.png');
-export const LOGO = require('@/assets/logo.png');
+export * from './images';
